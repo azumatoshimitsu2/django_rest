7a9c8432238fc1a877e68fab5ee4f837764e1a25
敏満 東 <kangaroonote@gmail.com>
2019-12-08 17:47:42 +0900

    View 更新

--- rest/rest_api/urls.py
+++ rest/rest_api/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from rest_api import views as api_views
+from . import views as api_views
 
 urlpatterns = [
     #本モデルの取得（⼀覧） ・登録  
-    path('rest_api/books/', api_views.BookListCreateAPIView.as_view()),
+    # path('books/', api_views.BookListCreateAPIView),
     #本モデルの取得（詳細） ・更新・⼀部更新・削除
-    path('rest_api/books/<pk>/', api_views.BookRetrieveUpdateDestroyAPIView.as_view()),
+    #path('books/<pk>/', api_views.BookRetrieveUpdateDestroyAPIView.as_view()),
     ] 
+
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+router = routers.SimpleRouter()
+router.register('books', views.BookViewSet)
+
+urlpatterns = [
+    #すべてのアクション （⼀覧・詳細 ・登録・更新・⼀部更新・ 削除） をまとめて追加
+    path('api/', include(router.urls)),
+] 
+
