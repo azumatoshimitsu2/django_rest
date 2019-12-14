ef562c1e6f37c1160d99c697986d839ddab4078f
敏満 東 <kangaroonote@gmail.com>
2019-12-14 11:44:50 +0900

    API ドキュメント追加

--- rest/rest/settings.py
+++ rest/rest/settings.py
@@ -111,7 +111,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2, 
+    'PAGE_SIZE': 2,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema', 
 }
 
 # Internationalization
--- rest/rest_api/urls.py
+++ rest/rest_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from . import views as api_views
+from django.conf import settings
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
+from . import views as api_views
 from . import views
 
 router = routers.SimpleRouter()
@@ -11,4 +13,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
 ] 
+
+if settings.DEBUG:
+    urlpatterns += [path('docs/', include_docs_urls(title='API ドキュメント'))] 
 
