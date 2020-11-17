40616b116cab9eb2388df083c5736b366c44859b
Azuma Toshimitsu <azuma@caracriworks.com>
2020-11-17 14:19:24 +0900

    django_nuxt

--- src/api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets 
-
-from .models import Sample
-from .serializers import SampleSerializer
-
-class SampleViewSet(viewsets.ModelViewSet): 
-    queryset = Sample.objects.all()
-    serializer_class = SampleSerializer 
